ffd187b21c9bbf58bce9c217a5ccb1ae54576373
razu ahammed <razuahammed7979@gmail.com>
2023-07-05 00:22:41 +0600

    for practise

---
+++ day2.js
@@ -0,0 +1,13 @@
+//loop in object
+
+let obj = {
+    Bangladesh: "Dhaka",
+    India: "Delhi",
+    Nepal: "Kathmandu",
+    Afganistan: "Kabul",
+    Thailand: "Bangkok"
+}
+
+for(countryName in obj) {
+    console.log(countryName);
+}
